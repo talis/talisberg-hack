f34e0342c84897e8be9013e23c19d1ad71342e84
Richard Gubby <rgubby@googlemail.com>
2014-05-29 14:29:16 +0100

    Added default images

--- app/javascript/Main.js
+++ app/javascript/Main.js
@@ -6,53 +6,34 @@ var Main =
 
 };
 
+Main.images = ["app/images/tvlogo.png", "app/images/tvlogo2.png"];
+
 Main.onLoad = function()
 {
-    // Enable key event processing
-    this.enableKeys();
     widgetAPI.sendReadyEvent();
+    
+    // Load images
+    var i = 0;
+    
+    
+    
+    for(var image in this.images){
+//    	alert(this.images[image]);
+    }
+    
+    
+    // Check for client ID & client secret
+//    var clientId = localStorage.getItem('clientId');
+//    alert("clientid:"+clientId);
+//    if(!clientId){
+//    	$('#clientId').focus();
+//    }
+//    localStorage.setItem('test', 'YO DAWG');
+//    alert(localStorage.getItem('test'));
+    
 };
 
 Main.onUnload = function()
 {
 
-};
-
-Main.enableKeys = function()
-{
-    document.getElementById("anchor").focus();
-};
-
-Main.keyDown = function()
-{
-    var keyCode = event.keyCode;
-    alert("Key pressed: " + keyCode);
-
-    switch(keyCode)
-    {
-        case tvKey.KEY_RETURN:
-        case tvKey.KEY_PANEL_RETURN:
-            alert("RETURN");
-            widgetAPI.sendReturnEvent();
-            break;
-        case tvKey.KEY_LEFT:
-            alert("LEFT");
-            break;
-        case tvKey.KEY_RIGHT:
-            alert("RIGHT");
-            break;
-        case tvKey.KEY_UP:
-            alert("UP");
-            break;
-        case tvKey.KEY_DOWN:
-            alert("DOWN");
-            break;
-        case tvKey.KEY_ENTER:
-        case tvKey.KEY_PANEL_ENTER:
-            alert("ENTER");
-            break;
-        default:
-            alert("Unhandled key");
-            break;
-    }
 };
